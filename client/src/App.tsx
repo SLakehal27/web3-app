59b4c14eb435e9d21669863389db3520ea0b16bd
Mathieu <mathieub9359@gmail.com>
2024-03-29 22:39:40 -0400

    Functional version for connecting client to backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,7 @@ import { methodForTesting, web3 } from "./utils/contract";
 import { Address } from "web3";
 
 function App() {
-
-  const handleClick = async () => {
-    console.log(web3)
-
-    const accounts = await web3.eth.requestAccounts()
-    const address: Address = accounts[0]
-
-    methodForTesting(address)
-  };
-
-
-  return <h1 className="text-emerald-300" onClick={handleClick}>Hello world :D</h1>;
+  return <h1 className="text-emerald-300" onClick={methodForTesting}>Hello world :D</h1>;
 }
 
 export default App;
