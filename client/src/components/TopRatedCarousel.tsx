0d906ac95c183205229685da40e84d6557eb310f
Selim Lakehal <selim.lakehal@polymtl.ca>
2024-03-31 03:56:19 -0400

    Added averageRating to TopRatedCarousel

--- client/src/components/TopRatedCarousel.tsx
+++ client/src/components/TopRatedCarousel.tsx
@@ -21,11 +21,16 @@ export function TopRatedCarousel({ movies }: { movies: Movie[] | null }) {
       <CarouselContent>
         {movies?.map((movie: Movie, index: number) => (
           <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/4">
-            <Link to={`review/${movie.id}`}>
-              <div className="p-1">
-                <MovieCard movie={movie} />
-              </div>
-            </Link>
+            <div className="flex flex-col items-center">
+              <p className="font-bold mt-6 text-3xl">
+                {movie.averageRating.toString()}
+              </p>
+              <Link to={`review/${movie.id}`}>
+                <div className="p-1">
+                  <MovieCard movie={movie} />
+                </div>
+              </Link>
+            </div>
           </CarouselItem>
         ))}
       </CarouselContent>
