0f46d56f8cfc5425ab3331e68e4a76b90f52c7c7
Andreea Maria <andreeamariaghioltan@gmail.com>
2024-03-30 20:25:50 -0400

    More beautiful table

--- client/src/pages/MovieListPage.tsx
+++ client/src/pages/MovieListPage.tsx
@@ -39,20 +39,35 @@ export function MovieListPage() {
   );
 }
 
-const TableView: FC<{ data: Movie[] }> = ({ data }) =>{
-  return(
-<table>
-  <tbody>
-    {data.map(movie => (
-    <tr>
-    <td>{movie.rank}</td>
-    <td>{movie.title}</td>
-    <td>{movie.overallRating}</td>
-    <td>{movie.rating}</td>
-    </tr>
-    ))}
-  </tbody>
-</table>
-  )
-}
-
+const TableView: FC<{ data: Movie[] }> = ({ data }) => {
+  return (
+    <table className="min-w-full divide-y divide-gray-200">
+      <thead className="bg-gray-50">
+        <tr>
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Rank
+          </th>
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Title
+          </th>
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Overall Rating
+          </th>
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Rating
+          </th>
+        </tr>
+      </thead>
+      <tbody className={`bg-white divide-y divide-gray-200`}>
+        {data.map((movie, index) => (
+          <tr key={index} className={`${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}`}>
+            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{movie.rank}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{movie.title}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{movie.overallRating}</td>
+            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{movie.rating}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
