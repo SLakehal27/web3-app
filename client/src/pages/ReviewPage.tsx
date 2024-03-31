0db5706277a7a9eddcda473b978e81f4c847adcb
Selim Lakehal <selim.lakehal@polymtl.ca>
2024-03-30 21:10:04 -0400

    Added start of review page

--- client/src/pages/ReviewPage.tsx
+++ client/src/pages/ReviewPage.tsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import { Button } from "../components/ui/button";
+
 export function ReviewPage() {
+  const [rating, setRating] = useState(5);
   return (
-    <>
-      <p>Add ratings here</p>
-    </>
+    <div>
+      <div className="flex flex-col justify-center items-center mt-36">
+        <p>Movie Title</p>
+        <p>Description</p>
+        <p>Rating</p>
+      </div>
+
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex gap-2 items-center">
+          <input
+            type="range"
+            min={1}
+            max={10}
+            value={rating}
+            onChange={(e) => setRating(e.target.valueAsNumber)}
+          />
+          <p>{rating}</p>
+        </div>
+        <Button className="bg-indigo-500 hover:bg-indigo-800 hover:scale-110 transition">
+          Add rating
+        </Button>
+
+        <Button className="bg-indigo-500 hover:bg-indigo-800 hover:scale-110 transition">
+          Add to Watch List
+        </Button>
+      </div>
+    </div>
   );
 }
