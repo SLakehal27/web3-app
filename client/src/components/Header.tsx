b28d434f3d34dfcd74d0ab0ff2347b3d1e2feead
Selim Lakehal <selim.lakehal@polymtl.ca>
2024-03-31 01:28:41 -0400

    Centered movie titles and description in main page

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,14 +4,18 @@ import Logo from "../assets/media/cinemaApp.png";
 
 export function Header() {
   return (
-    <header className="bg-purple-200 p-10">
-      <div className="flex justify-between items-center">
+    <header className="bg-purple-200">
+      <div className="flex justify-between items-center p-2">
         <Link to="/">
-        <img src={Logo} style= {{height:"150px"}}/> 
+          <img
+            src={Logo}
+            alt="logo"
+            className="h-[100px] hover:scale-110 transition"
+          />
         </Link>
         <Link to="/movielist">
           <Button
-            className="bg-indigo-500 hover:bg-indigo-800 hover:scale-110 transition"
+            className="bg-indigo-500 hover:bg-indigo-800 hover:scale-110 transition mr-5"
             variant={"default"}
           >
             My Movie List
--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -5,10 +5,10 @@ export function MovieCard({ movie }: { movie: Movie }) {
   return (
     <div>
       <Card className="m-10 flex flex-col w-[250px] items-center justify-between hover:scale-110 transition h-[350px]">
-        <CardHeader>
+        <CardHeader className="font-bold text-xl text-center">
           <h1>{movie.title}</h1>
         </CardHeader>
-        <CardContent className="overflow-y-auto">
+        <CardContent className="overflow-y-auto text-center">
           <p>{movie.description}</p>
         </CardContent>
         <CardFooter>
