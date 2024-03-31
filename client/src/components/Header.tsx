a3f8463d6a5dc690a40f6dc2582126a6ce4134fc
Andreea Maria <andreeamariaghioltan@gmail.com>
2024-03-30 20:54:46 -0400

    Add logo to header and align header elements

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
+import Logo from "../assets/media/cinemaApp.png";
 
 export function Header() {
   return (
     <header className="bg-purple-200 p-10">
-      <div className="flex justify-between">
-        <p>Header Logo</p>
+      <div className="flex justify-between items-center">
+        <img src={Logo} style= {{height:"150px"}}/> 
         <Link to="/movielist">
           <Button
             className="bg-indigo-500 hover:bg-indigo-800 hover:scale-110 transition"
